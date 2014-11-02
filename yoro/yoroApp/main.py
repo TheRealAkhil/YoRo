35fcbb576d00ab4e240776b1c224a67f61342591
Akhil Nistala <anistala@mit.edu>
2014-11-02 04:58:57 -0500

    changed a method

--- yoro/yoroApp/main.py
+++ yoro/yoroApp/main.py
@@ -9,17 +9,18 @@ import get_yo_main
 
 def database_query():
 	# for each request in the database
-	# use make_date_object to make a datetime obj
+	# get the datetime obj
 	# see if datetime.now() > datetime obj
 	# if Yes, then run send_first_yo(username)
 	# and then run the main method
+	print "in"
 	for n in Note.objects.all():
 		d = n.time
 		if datetime.now() > d:
 			send_first_yo(n.user)
 
 
-schedule.every(5).minutes.do(database_query)
+schedule.every(1).minutes.do(database_query)
 
 while True:
 	schedule.run_pending()
@@ -34,13 +35,9 @@ def send_first_yo(username):
 	requests.post(api_address, data={'api_token': api_token, 'username': username});
 	return
 
-
-def make_date_object(date, time):
-	return datetime.combine(date, time)
-
 app = Flask(__name__)
 
-@app.route("/yo/")
+@app.route("/yo")
 def main_yo():
 	username = request.args.get('username')
 	location = request.args.get('location')
@@ -65,4 +62,4 @@ def main_yo():
 
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000)
+  app.run(host="0.0.0.0", port=8000)
