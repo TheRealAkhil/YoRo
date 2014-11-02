5f5e2399751f68bee0969e32997b015b9f1f27db
Akhil Nistala <anistala@mit.edu>
2014-11-02 02:17:00 -0500

    modifying main

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ from datetime import datetime
 from API_KEY import api_token, api_address
 from flask import request, Flask
 import requests
+import schedule
+import time
+
+def database_query():
+	# for each request in the database
+	# use make_date_object to make a datetime obj
+	# see if datetime.now() > datetime obj
+	# if Yes, then run send_first_yo(username)
+	# and then run the main method
+
+schedule.every(5).minutes.do(database_query)
+
+while True:
+	schedule.run_pending()
+	time.sleep(1)
+
 
 def setup(host, path):
 	url = 'http://{0}{1}'.format(host, path)
-
-
-def get_current_date():
-	return datetime.now()
 
 
 def send_yo_with_link(username, link):
@@ -16,7 +28,7 @@ def send_yo_with_link(username, link):
 	return
 
 
-def send_yo(username):
+def send_first_yo(username):
 	requests.post(api_address, data={'api_token': api_token, 'username': username});
 	return
 
@@ -38,8 +50,6 @@ def get_location():
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000)
 
-# send_yo_with_link('ultraman', 'http://www.nba.com')
-# get_location()
 
 
 
