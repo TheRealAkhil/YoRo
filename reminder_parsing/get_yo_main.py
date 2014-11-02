d6476def9720212920cfe1a4f4922d84116dcc9e
tejassun@mit.edu <Tejas@Tejass-MacBook-Pro.local>
2014-11-02 02:57:04 -0500

    updated get_time function for yo_main

--- reminder_parsing/get_yo_main.py
+++ reminder_parsing/get_yo_main.py
@@ -8,17 +8,22 @@ Returns a datetime for when the Yo should be sent along with the corresponding y
 URL for Yelp matching location within 4000 meters of the area
 """
 
+def get_datetime(reminder_text):
+	date = get_category.get_date(text)
+	time = get_category.get_time(text)
+	return datetime.datetime(date[2],date[0],date[1],time[0],time[1])
+
 def get_yo_main(latitude, longitude, reminder_text):
 	reminder_parsed = gc.get_reminder_data(reminder_text)
 	category = reminder_parsed["category"] + " " + reminder_parsed["special"]
-	datetime = reminder_parsed["datetime"]
 	if category != "":
 		url = yelp.get_yelp_recommendation(category, latitude, longitude)
-		return url, datetime
+		return url
 	else:
 		return "Not Processable"
 
 get_yo_main(41.309882, -72.933874, "i need mexican restaurant for 11/30 1:30 am")
+
 
 
 
